f5ca2c55e543a04cab279245fddfe6b946f59e5a
esfomeado <vps1992@live.com.pt>
2023-01-03 21:25:37 +0000

    New bug

--- src/test/java/com/example/bug/FixtureMonkeyTest.java
+++ src/test/java/com/example/bug/FixtureMonkeyTest.java
@@ -10,6 +10,7 @@ import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.LabMonkey;
 import com.navercorp.fixturemonkey.api.generator.ArbitraryContainerInfo;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+import com.navercorp.fixturemonkey.jackson.introspector.JacksonArbitraryIntrospector;
 
 class FixtureMonkeyTest
 {
@@ -29,6 +30,23 @@ class FixtureMonkeyTest
         Workflow workflow = fixtureMonkey.giveMeBuilder( Workflow.class )
                                          .set( "states[0].triggers[0].conditions", List.of( condition ) )
                                          .sample();
+
+        assertThat( workflow ).isNotNull();
+    }
+
+    @Test
+    void invalidIntrospector()
+    {
+        FixtureMonkey fixtureMonkey = LabMonkey.labMonkeyBuilder()
+                                               .objectIntrospector( FieldReflectionArbitraryIntrospector.INSTANCE )
+                                               .defaultArbitraryContainerInfo( new ArbitraryContainerInfo( 1, 1, false ) )
+                                               .nullableContainer( false )
+                                               .nullableElement( false )
+                                               .pushAssignableTypeArbitraryIntrospector( Trigger.class, JacksonArbitraryIntrospector.INSTANCE )
+                                               .interfaceImplements( Condition.class, List.of( BasicCondition.class, ValueCondition.class ) )
+                                               .build();
+
+        Workflow workflow = fixtureMonkey.giveMeOne( Workflow.class );
 
         assertThat( workflow ).isNotNull();
     }
