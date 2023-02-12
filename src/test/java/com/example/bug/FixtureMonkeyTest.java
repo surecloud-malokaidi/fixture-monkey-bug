364dc4e85a4e20afec2fdbde19e029cf18b863a3
esfomeado <vps1992@live.com.pt>
2023-02-12 11:36:14 +0000

    New bugs

--- src/test/java/com/example/bug/FixtureMonkeyTest.java
+++ src/test/java/com/example/bug/FixtureMonkeyTest.java
@@ -1,8 +1,11 @@
 package com.example.bug;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +13,6 @@ import com.navercorp.fixturemonkey.FixtureMonkey;
 import com.navercorp.fixturemonkey.LabMonkey;
 import com.navercorp.fixturemonkey.api.generator.ArbitraryContainerInfo;
 import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
-import com.navercorp.fixturemonkey.jackson.introspector.JacksonArbitraryIntrospector;
 
 class FixtureMonkeyTest
 {
@@ -23,31 +25,27 @@ class FixtureMonkeyTest
                                            .build();
 
     @Test
-    void bugTest()
+    void mockTest()
     {
-        ValueCondition condition = fixtureMonkey.giveMeOne( ValueCondition.class );
+        UUID id = UUID.randomUUID();
+        ValueCondition condition = mock( ValueCondition.class );
+
+        given( condition.getId() ).willReturn( id );
+
+        assertThat( condition.getId() ).isEqualTo( id );
 
         Workflow workflow = fixtureMonkey.giveMeBuilder( Workflow.class )
                                          .set( "states[0].triggers[0].conditions", List.of( condition ) )
                                          .sample();
 
-        assertThat( workflow ).isNotNull();
+        assertThat( workflow.getStates().get( 0 ).getTriggers().get( 0 ).getConditions().get( 0 ).getId() ).isEqualTo( id );
     }
 
     @Test
-    void invalidIntrospector()
+    void entityTest()
     {
-        FixtureMonkey newFixtureMonkey = LabMonkey.labMonkeyBuilder()
-                                               .objectIntrospector( FieldReflectionArbitraryIntrospector.INSTANCE )
-                                               .defaultArbitraryContainerInfo( new ArbitraryContainerInfo( 1, 1, false ) )
-                                               .nullableContainer( false )
-                                               .nullableElement( false )
-                                               .pushAssignableTypeArbitraryIntrospector( Trigger.class, JacksonArbitraryIntrospector.INSTANCE )
-                                               .interfaceImplements( Condition.class, List.of( BasicCondition.class, ValueCondition.class ) )
-                                               .build();
+        Entity entity = fixtureMonkey.giveMeOne( Entity.class );
 
-        Workflow workflow = newFixtureMonkey.giveMeOne( Workflow.class );
-
-        assertThat( workflow ).isNotNull();
+        assertThat( entity ).isNotNull();
     }
 }
